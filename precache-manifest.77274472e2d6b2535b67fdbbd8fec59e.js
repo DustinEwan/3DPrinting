cf53bdc3282b93e620b8c53167aa973bd6d2eb04
Dustin Ewan <dustin.ewan@illumisoft.com>
2020-08-03 06:08:40 -0500

    Updates

---
+++ precache-manifest.77274472e2d6b2535b67fdbbd8fec59e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c610f205c728c79189b4e086500be86e",
+    "url": "/3DPrinting/index.html"
+  },
+  {
+    "revision": "49e2d825560f1788fdb0",
+    "url": "/3DPrinting/static/css/2.e7180fea.chunk.css"
+  },
+  {
+    "revision": "455e77377c92d6eb59c8",
+    "url": "/3DPrinting/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49e2d825560f1788fdb0",
+    "url": "/3DPrinting/static/js/2.9bfaa756.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/3DPrinting/static/js/2.9bfaa756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455e77377c92d6eb59c8",
+    "url": "/3DPrinting/static/js/main.0ee81fcc.chunk.js"
+  },
+  {
+    "revision": "0d96d3c41cff7771fe1b",
+    "url": "/3DPrinting/static/js/runtime-main.32fe7029.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1938a6fd6414f791a3b44ff3c5790c04.js"
+  "/3DPrinting/precache-manifest.77274472e2d6b2535b67fdbbd8fec59e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/3DPrinting/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
